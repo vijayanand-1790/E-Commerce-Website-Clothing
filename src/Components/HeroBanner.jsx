a8ba34e33446cfaa967bc8127a39959a986eb18f
Vijay Anand <vijayvjrj@gmail.com>
2025-06-28 21:48:57 +0530

    Modified: modified some images

--- src/Components/HeroBanner.jsx
+++ src/Components/HeroBanner.jsx
@@ -4,7 +4,7 @@ import HeroPexel from '../assets/HeroPexel.jpg'
 const HeroBanner = () => {
   return (
     <div className='bg-white mt-20 mb-10 px-5'>
-      <div className='relative max-w-7xl mx-auto rounded-2xl md:h-[600px] h-[500px] flex justify-center items-center bg-cover bg-center' style={{ backgroundImage: `url(${HeroPexel})`, backgroundPosition: 'top', }}>
+      <div className='relative max-w-7xl mx-auto rounded-2xl md:h-[600px] h-[500px] flex justify-center items-center bg-cover bg-center' style={{ backgroundImage: `url(${HeroPexel})`, backgroundPosition: 'center', }}>
         <div className='absolute inset-0 bg-black/50 rounded-2xl flex justify-center items-center'>
           <div className='text-center text-white space-y-4'>
             <h1 className='lg:text-7xl md:text-6xl text-4xl font-bold flex'>Discover your style!</h1>
